006f45d22370cb6d143e1900713f2a9e7b0cae25
Skyser <skyseroma@gmail.com>
2016-11-17 02:13:49 +0300

    lesson 3

--- Snake1/Snake1/Program.cs
+++ Snake1/Snake1/Program.cs
@@ -10,7 +10,18 @@ namespace Snake1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            point p1 = new point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
+
+            point p2 = new point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+            p2.Draw();
+
             Console.ReadLine();
         }
     }
