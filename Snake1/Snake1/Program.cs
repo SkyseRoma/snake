3bf2fe3152dd4de21d4df347c6fa3c71b0bbef1e
Skyser <skyseroma@gmail.com>
2016-11-18 01:18:01 +0300

    les7

--- Snake1/Snake1/Program.cs
+++ Snake1/Snake1/Program.cs
@@ -10,7 +10,29 @@ namespace Snake1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            point p1 = new point(1, 3, '*');
+            p1.Draw();
+
+            point p2 = new point(4, 5, '#');
+            p2.Draw();
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+            List<point> plist = new List<point>();
+            plist.Add(p1);
+            plist.Add(p2);
+
             Console.ReadLine();
         }
     }
